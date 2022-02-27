e02c6c8498d242bd90884ed996773fea9b42347b
Aldin <aldin.bijedic@gmx.at>
2022-02-27 22:51:32 +0100

    Update app.component.ts

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,17 @@
-import { Component, HostListener } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import 'aos/dist/aos.css';
+import * as Aos from 'aos';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+
   title = 'portfolio';
 
+  ngOnInit(): void {
+    Aos.init();
+  }
 }
