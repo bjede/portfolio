db6461030413ce1fad93967f9305e750e83398b2
Aldin <aldin.bijedic@gmx.at>
2022-02-27 13:57:49 +0100

    added skills component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,12 +6,14 @@ import { AppComponent } from './app.component';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { HeaderComponent } from './header/header.component';
 import { MainMenuComponent } from './main-menu/main-menu.component';
+import { SkillsComponent } from './skills/skills.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    MainMenuComponent
+    MainMenuComponent,
+    SkillsComponent
   ],
   imports: [
     BrowserModule,
