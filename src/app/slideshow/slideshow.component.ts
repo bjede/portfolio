e0505ca1bd203e87e96ea5c1e8021027b28a7b38
Aldin <aldin.bijedic@gmx.at>
2022-03-01 22:59:23 +0100

    added Typewriter

--- src/app/slideshow/slideshow.component.ts
+++ src/app/slideshow/slideshow.component.ts
@@ -1,5 +1,7 @@
+import { ViewportScroller } from '@angular/common';
 import { Component, OnInit } from '@angular/core';
-
+// @ts-ignore
+import Typewriter from 't-writer.js';
 @Component({
   selector: 'app-slideshow',
   templateUrl: './slideshow.component.html',
@@ -10,17 +12,51 @@ export class SlideshowComponent implements OnInit {
   images = ['bg1', 'bg2', 'bg3', 'bg4'];
   currentImage = 0;
 
-  constructor() { }
+  constructor(public viewPortScroller: ViewportScroller) { }
 
   ngOnInit(): void {
     this.slideShow();
+
+    const target1 = document.querySelector('.tw1');
+    const target2 = document.querySelector('.tw2')
+
+    const writer1 = new Typewriter(target1, {
+      loop: false,
+      typeSpeed: 60,
+      typeColor: 'white',
+      cursorColor: 'white'
+    })
+
+    const writer2 = new Typewriter(target2, {
+      loop: false,
+      typeSpeed: 60,
+      typeColor: 'white',
+      cursorColor: 'white'
+    })
+
+    writer1
+      .rest(1500)
+      .type('i\'am a frontend ')
+      .removeCursor()
+      .then(writer2.start.bind(writer2))
+      .start()
+
+    writer2
+      .changeTypeColor('var(--c-primary)')
+      .type("developer.")
+      .rest(2000)
+      .removeCursor()
   }
 
   slideShow() {
-    setInterval(()=> {
+    setInterval(() => {
       this.currentImage++;
       this.currentImage = this.currentImage % this.images.length;
     }, 7000);
+  }
+
+  scrollToPosition(id: string){
+    this.viewPortScroller.scrollToAnchor(id);
   }
 
 }
