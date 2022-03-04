2ab228e10e55be5ecb9858c9b7c8e4d161aeb513
Aldin <aldin.bijedic@gmx.at>
2022-03-04 23:10:14 +0100

    Created Routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,27 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AboutMeComponent } from './about-me/about-me.component';
+import { ContactComponent } from './contact/contact.component';
+import { FooterComponent } from './footer/footer.component';
+import { HeaderComponent } from './header/header.component';
+import { ImprintComponent } from './imprint/imprint.component';
+import { MyWorkComponent } from './my-work/my-work.component';
+import { SkillsComponent } from './skills/skills.component';
+import { SlideshowComponent } from './slideshow/slideshow.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: HeaderComponent },
+  { path: '', component: SkillsComponent, outlet: 'test' },
+  { path: '', component: MyWorkComponent },
+  { path: '', component: FooterComponent },
+  { path: '', component: SlideshowComponent },
+  { path: '', component: ContactComponent },
+  { path: '', component: AboutMeComponent },
+  { path: 'imprint', component: ImprintComponent },
+
+
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
