eabee0cd222217e3120c60ab4a013044c126b000
Aldin <aldin.bijedic@gmx.at>
2022-03-04 23:11:11 +0100

    Import Router

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import 'aos/dist/aos.css';
 import * as Aos from 'aos';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-root',
@@ -11,6 +12,8 @@ export class AppComponent implements OnInit {
 
   title = 'portfolio';
 
+  constructor(public router: Router) {}
+  
   ngOnInit(): void {
     Aos.init();
   }
